329c180599ea91ef62c9aa5b0d69db963a288410
sandrineuwumukiza <uwasandrine99@gmail.com>
2024-02-24 23:11:10 -0700

    adding traing day on my current games

---
+++ secretmessage.js
@@ -0,0 +1,6 @@
+// GENERATING SECRET MESSAGE
+
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get',
+ 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can',
+  'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+  console.log(secretMessage.length);
