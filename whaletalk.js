aed9e1a41d4ea0698f5dad29cca8a07c101d9822
sandrineuwumukiza <uwasandrine99@gmail.com>
2024-02-25 01:35:46 -0700

    adding whaletalk to change our to whale talk

---
+++ whaletalk.js
@@ -0,0 +1,23 @@
+// WHALE TALK// Step 1: Create a variable named input
+const input = 'turpentine and turtles';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+let resultArray = [];
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+      if (input[i] === 'e') {
+        resultArray.push(input[i]);
+      }
+      if (input[i] === 'u') {
+        resultArray.push(input[i]);
+      }
+    }
+  }
+}
+
+console.log(resultArray);
+const resultString = resultArray.join('').toUpperCase();
+console.log(resultString);
+
