0ccfe4e10c157007b82f29bbe8e67b1bd738b56c
sandrineuwumukiza <uwasandrine99@gmail.com>
2024-02-25 23:52:04 -0700

    adding mealer maker to my project

---
+++ mealermaker.js
@@ -0,0 +1,36 @@
+// MEALER MAKER
+
+const menu = {
+    _meal: '',
+    _price: 0,
+    
+    set meal(mealToCheck) {
+      if (typeof mealToCheck === 'string') {
+        this._meal = mealToCheck;
+      }
+    },
+    
+    set price(priceToCheck) {
+      if (typeof priceToCheck === 'number') {
+        this._price = priceToCheck;
+      }
+    },
+    
+    get todaysSpecial() {
+      if (this._meal && this._price) {
+        return `Today’s Special is ${this._meal} for $${this._price}!`;
+      } else {
+        return 'Meal or price was not set correctly!';
+      }
+    }
+  };
+  
+  
+  menu._meal = 123;
+  menu._price = 'Not a number'; 
+  console.log(menu);
+  menu.meal = 'Spaghetti';
+  menu.price = 5;
+  console.log(menu);
+  console.log(menu.todaysSpecial);
+  
